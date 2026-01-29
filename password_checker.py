9ff28565e5ef99d330cd2a601853f783fd5c96f6
cybersec-apprentice <154281550+cybersec-apprentice@users.noreply.github.com>
2026-01-29 00:09:03 +0000

    Update password_checker.py

--- password_checker.py
+++ password_checker.py
@@ -1,33 +1,35 @@
 def check_password_strength(password):
-  has_upper = False
-  has_lower = False
-  has_number = False
+    has_upper = False
+    has_lower = False
+    has_number = False
 
-  for char in password:
-    if char.isupper():
-      has_upper = True
-    elif char.islower():
-      has_lower = True
-    elif char.isdigit()
-      has_number = True
+    for char in password:
+        if char.isupper():
+            has_upper = True
+        elif char.islower():
+            has_lower = True
+        elif char.isdigit():
+            has_number = True
 
-  if len(password) < 10:
-    return "Password must be at least 10 characters long."
-  if " " in password:
-    return "Password cannot contain spaces."
-  if not has_upper:
-    return "Password must inlude an uppercase letter"
-  if not has_lower:
-    return "Password must include a lowercase letter"
-  if not has_number:
-    return "Password must include a number"
+    if len(password) < 10:
+        return "Password must be at least 10 characters long."
+    if " " in password:
+        return "Password cannot contain spaces."
+    if not has_upper:
+        return "Password must inlude an uppercase letter"
+    if not has_lower:
+        return "Password must include a lowercase letter"
+    if not has_number:
+        return "Password must include a number"
 
-  return "Password meets all requirements"
+    return "Password meets all requirements"
+
 
 def main():
-  password = input("Enter Password")
-  result = check_password_strength(password)
-  print(result)
+    password = input("Enter Password: ")
+    result = check_password_strength(password)
+    print(result)
 
-if __name__ == "__mnain__":
-  main()
+
+if __name__ == "__main__":
+    main()
